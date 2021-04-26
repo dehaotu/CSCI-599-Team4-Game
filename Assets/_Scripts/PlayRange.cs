74a277854076a6a1932b7da92c676840c403d5fd
YaqiHan <hanyaqi141@gmail.com>
2021-04-26 00:10:47 -0700

    Add Evil Lord and Golem Audio
    - Add Play Range C# and audio source in EvilLord and Golem prefab
    - Player will hear audio within the certain range from the monster

---
+++ Assets/_Scripts/PlayRange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRange : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    void OnTriggerEnter(Collider collider)
+    {
+
+        if (collider.tag == "Player" && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+
+}
